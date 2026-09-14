02e0f3bb7b10bb3ce82c136d6bc7aa8484b857fe
Regis Balla <ballaregis2008@gmail.com>
2026-09-14 10:20:32 +0200

    implement MySQL connection pool using environment variables

---
+++ src/lib/server/database.js
@@ -0,0 +1,16 @@
+// MySQL Connection Pool
+import mysql from 'mysql2/promise';
+import {
+    DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD
+} from '$env/static/private';
+
+// Connection Pool erstellen — wiederverwendbare DB-Verbindungen
+const pool = mysql.createPool({
+    host: DB_HOST,
+    port: DB_PORT,
+    database: DB_NAME,
+    user: DB_USER,
+    password: DB_PASSWORD
+});
+
+export default pool;
